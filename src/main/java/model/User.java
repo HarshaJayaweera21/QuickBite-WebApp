b4fbd7f2ed9409ca36f4f2bd0fa8f43ca1ca2663
Sandaruwan Shanaka <perolopessthesoosa@gmail.com>
2025-04-15 12:07:15 +0530

    Implement user profile management methods and CSV serialization in User class

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -31,3 +31,47 @@ public abstract class User {
     public String getAddress() { return address; }
     public void setAddress(String address) { this.address = address; }
 
+    // Abstract methods to be implemented by subclasses
+    public abstract String getRole();
+
+    // Common methods
+    public void register() {
+        // To be implemented in UserService
+    }
+
+    public boolean login(String email, String password) {
+        return this.email.equals(email) && this.password.equals(password);
+    }
+
+    public void updateProfile(String name, String email, String password, String phoneNumber, String address) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public void deleteAccount() {
+        // To be implemented in UserService
+    }
+
+    public void viewOrderHistory() {
+        // To be implemented in UserService
+    }
+
+    // CSV serialization
+    public String toCSV() {
+        return String.format("%s,%s,%s,%s,%s,%s,%s",
+                userID, getRole(), name, email, password, phoneNumber, address);
+    }
+
+    public static User fromCSV(String csvLine) {
+        String[] data = csvLine.split(",");
+        if ("Customer".equals(data[1])) {
+            return new Customer(data[0], data[2], data[3], data[4], data[5], data[6]);
+        } else if ("Admin".equals(data[1])) {
+            return new Admin(data[0], data[2], data[3], data[4], data[5], data[6]);
+        }
+        return null;
+    }
+}
